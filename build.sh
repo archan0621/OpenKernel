da8cb063d4af8b6ec7f899411056859b9ded3ffd
박종하 <joshua_0621@naver.com>
2026-01-23 16:14:40 +0900

    build: disable SSE/MMX instructions to prevent invalid opcode exceptions

--- build.sh
+++ build.sh
@@ -13,16 +13,16 @@ cp res/grub.cfg build/iso/boot/grub/grub.cfg
 nasm -f elf32 src/boot.asm -o build/boot.o
 
 # 2) kernel.c to object
-x86_64-elf-gcc -m32 -ffreestanding -O2 -Iinclude -c src/kernel.c -o build/kernel.o
+x86_64-elf-gcc -m32 -ffreestanding -O2 -Iinclude -mno-sse -mno-sse2 -mno-mmx -c src/kernel.c -o build/kernel.o
 
 # 3) video.c to object
-x86_64-elf-gcc -m32 -ffreestanding -O2 -Iinclude -c src/drivers/video/video.c -o build/video.o
+x86_64-elf-gcc -m32 -ffreestanding -O2 -Iinclude -mno-sse -mno-sse2 -mno-mmx -c src/drivers/video/video.c -o build/video.o
 
 # 4) font8x16.c to object
-x86_64-elf-gcc -m32 -ffreestanding -O2 -Iinclude -c src/font/font8x16.c -o build/font8x16.o
+x86_64-elf-gcc -m32 -ffreestanding -O2 -Iinclude -mno-sse -mno-sse2 -mno-mmx -c src/font/font8x16.c -o build/font8x16.o
 
 # 5) console.c to object
-x86_64-elf-gcc -m32 -ffreestanding -O2 -Iinclude -c src/drivers/console/console.c -o build/console.o
+x86_64-elf-gcc -m32 -ffreestanding -O2 -Iinclude -mno-sse -mno-sse2 -mno-mmx -c src/drivers/console/console.c -o build/console.o
 
 # 6) link 
 x86_64-elf-ld -m elf_i386 -T linker.ld -o build/kernel.elf build/boot.o build/kernel.o build/video.o build/font8x16.o build/console.o
