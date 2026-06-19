216a57fb0d1990136aaa0c151ca1b04479ee4e20
박종하 <joshua_0621@naver.com>
2026-06-19 20:07:42 +0900

    feat: add channel receiver wait queue

--- include/process/channel.h
+++ include/process/channel.h
@@ -21,4 +21,5 @@ bool channel_recv(channel_t* channel, channel_message_t* out_message);
 uint32_t channel_get_id(const channel_t* channel);
 uint32_t channel_get_owner_pid(const channel_t* channel);
 uint32_t channel_get_count(const channel_t* channel);
-task_struct_t* channel_get_waiting_receiver(const channel_t* channel);
+uint32_t channel_get_waiting_receiver_count(const channel_t* channel);
+task_struct_t* channel_peek_waiting_receiver(const channel_t* channel);
